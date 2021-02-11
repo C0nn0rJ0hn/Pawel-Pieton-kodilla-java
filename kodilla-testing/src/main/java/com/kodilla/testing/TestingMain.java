a95396706ac226e1bc3720674fa9e71f9c609863
Paweł Piętoń <pawel.pieton@gmail.com>
2021-02-11 10:38:57 +0100

    6.2 Added Calculator class to kodilla-testing module

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,44 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if(result.equals("theForumUser"))
+        {
+            System.out.println("test OK");
+        }
+        else
+        {
+            System.out.println("Error!");
+        }
+        System.out.println();
+
+        Calculator calculator = new Calculator();
+
+        int addResult = calculator.add(50, 20);
+        int substructResult = calculator.substract(50, 20);
+
+        if(addResult == 70)
+        {
+            System.out.println("Calculator adding works properly - Test OK");
+        }
+        else
+        {
+            System.out.println("Test failed!");
+        }
+
+        if(substructResult == 30)
+        {
+            System.out.println("Calculator substruct works properly - Test OK");
+        }
+        else {
+            System.out.println("Test failed!");
+        }
     }
 }
