09bcf508aecc61559db90546fea8ad452d71caff
Paweł Piętoń <pawel.pieton@gmail.com>
2021-05-07 17:40:56 +0200

    17.3 Added manytomany package

--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
@@ -1,11 +1,14 @@
 package com.kodilla.hibernate.tasklist.dao;
 
+import com.kodilla.hibernate.task.Task;
+import com.kodilla.hibernate.task.TaskFinancialDetails;
 import com.kodilla.hibernate.tasklist.TaskList;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @SpringBootTest
@@ -15,7 +18,7 @@ public class TaskListDaoTestSuite
     private TaskListDao taskListDao;
 
     private static final String DESCRIPTION = "Description of list";
-    private static final String LISTNAME = "Name of the List";
+    private static final String LISTNAME = "ToDo List";
 
     @Test
     void testFindByListName()
@@ -33,6 +36,33 @@ public class TaskListDaoTestSuite
 
         //Cleanup
         int id = readListName.get(0).getId();
+        taskListDao.deleteById(id);
+    }
+
+    @Test
+    void testTaskListDaoSaveWithTasks()
+    {
+        //Given
+        Task task = new Task("Test: Learn Hibernate", 14);
+        Task task2 = new Task("Test: Write some entieties", 3);
+        TaskFinancialDetails tfd = new TaskFinancialDetails(new BigDecimal(20), false);
+        TaskFinancialDetails tfd2 = new TaskFinancialDetails(new BigDecimal(10), false);
+        task.setTaskFinancialDetails(tfd);
+        task2.setTaskFinancialDetails(tfd2);
+        TaskList taskList = new TaskList(LISTNAME, "ToDo tasks");
+        taskList.getTasks().add(task);
+        taskList.getTasks().add(task2);
+        task.setTaskList(taskList);
+        task2.setTaskList(taskList);
+
+        //When
+        taskListDao.save(taskList);
+        int id = taskList.getId();
+
+        //Then
+        Assertions.assertNotEquals(0, id);
+
+        //Cleanup
         taskListDao.deleteById(id);
     }
 }
