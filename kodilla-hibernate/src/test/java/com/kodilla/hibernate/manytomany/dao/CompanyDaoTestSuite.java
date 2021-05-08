0dfd6454fa78bae951600c835d2080e9dbcc53f3
Paweł Piętoń <pawel.pieton@gmail.com>
2021-05-08 21:19:02 +0200

    17.4 Added 17.4 exercise

--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/manytomany/dao/CompanyDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/manytomany/dao/CompanyDaoTestSuite.java
@@ -7,11 +7,16 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 @SpringBootTest
 public class CompanyDaoTestSuite
 {
     @Autowired
     private CompanyDao companyDao;
+
+    @Autowired
+    private EmployeeDao employeeDao;
 
     @Test
     void testSaveManyToMany()
@@ -55,6 +60,62 @@ public class CompanyDaoTestSuite
             companyDao.deleteById(softwareMachineId);
             companyDao.deleteById(dataMaestersId);
             companyDao.deleteById(greyMatterId);
+        }
+        catch (Exception e)
+        {
+
+        }
+    }
+
+    @Test
+    void testNamedQueries()
+    {
+        //Given
+        Employee johnSmith = new Employee("John", "Smith");
+        Employee annaConnor = new Employee("Anna", "Connor");
+        Employee andersonSmith = new Employee("Anderson", "Smith");
+
+        Company softwareMachine = new Company("Software Machine");
+        Company dataMaesters = new Company("Data Maesters");
+        Company softSchool = new Company("Soft School");
+
+        softwareMachine.getEmployees().add(johnSmith);
+        softwareMachine.getEmployees().add(andersonSmith);
+        dataMaesters.getEmployees().add(annaConnor);
+        softSchool.getEmployees().add(annaConnor);
+        softSchool.getEmployees().add(andersonSmith);
+
+        johnSmith.getCompanies().add(softwareMachine);
+        andersonSmith.getCompanies().add(softwareMachine);
+        andersonSmith.getCompanies().add(softSchool);
+        annaConnor.getCompanies().add(dataMaesters);
+        annaConnor.getCompanies().add(softSchool);
+
+        //When
+        companyDao.save(softwareMachine);
+        int softwareMachineId = softwareMachine.getId();
+        companyDao.save(dataMaesters);
+        int dataMaestersId = dataMaesters.getId();
+        companyDao.save(softSchool);
+        int softSchoolId = softSchool.getId();
+
+        List<Employee> searchByLastname = employeeDao.retrieveEmployeeByLastname("Smith");
+        List<Company> searchByFirstThreeChar = companyDao.retrieveCompanyWithThreeFirstChar("Sof");
+
+        //Then
+        Assertions.assertEquals(2, searchByLastname.size());
+        Assertions.assertEquals(2, searchByFirstThreeChar.size());
+        Assertions.assertEquals("Smith",searchByLastname.get(0).getLastname());
+        Assertions.assertNotEquals(0, softwareMachineId);
+        Assertions.assertNotEquals(0, dataMaestersId);
+        Assertions.assertNotEquals(0, softSchoolId);
+
+        //Cleanup
+        try
+        {
+            companyDao.deleteById(softwareMachineId);
+            companyDao.deleteById(softSchoolId);
+            companyDao.deleteById(dataMaestersId);
         }
         catch (Exception e)
         {
