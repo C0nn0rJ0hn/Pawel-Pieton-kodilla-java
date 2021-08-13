a469837a0ea3d6c33eded2f18943f599293a1277
Paweł Piętoń <pawel.pieton@gmail.com>
2021-08-13 13:59:41 +0200

    30.3 Module - Java test for SQL procedure

---
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredProcedureTestSuite.java
@@ -0,0 +1,37 @@
+package com.kodilla.jdbc;
+
+import org.junit.jupiter.api.Test;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StoredProcedureTestSuite {
+    @Test
+    void testUpdateVipLevels() throws SQLException {
+        //Given
+        DbManager dbManager = DbManager.getInstance();
+        String sqlUpdate = "UPDATE READERS SET VIP_LEVEL=\"Not set\"";
+        Statement statement = dbManager.getConnection().createStatement();
+        statement.executeUpdate(sqlUpdate);
+        String sqlCheckTable = "SELECT COUNT(*) AS HOW_MANY FROM READERS WHERE VIP_LEVEL=\"Not set\"";
+
+
+        //When
+        String sqlProcedureCall = "CALL UpdateVipLevels()";
+        statement.execute(sqlProcedureCall);
+        ResultSet resultSet = statement.executeQuery(sqlCheckTable);
+
+
+        //Then
+        int howMany = -1;
+        if (resultSet.next()) {
+            howMany = resultSet.getInt("HOW_MANY");
+        }
+        assertEquals(0, howMany);
+        resultSet.close();
+        statement.close();
+    }
+}
